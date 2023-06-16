220251bc65c5d2d386dde76c71b10f71058f48fb
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-15 21:29:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+3include <stdio.h>
+/**
+ * main - A program that prints a line using the puts fucntion
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programing is like building a multilingual puzzle");
+return (0);
+}
