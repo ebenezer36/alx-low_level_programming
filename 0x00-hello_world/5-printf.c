b18763c8cc65daec4d826e76bff4a9e983f63a15
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-15 22:48:38 -0700

    100-intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - A programming that print a line using printf function
  * return 0 (Success)
  */
-int main(void);
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return(0);
