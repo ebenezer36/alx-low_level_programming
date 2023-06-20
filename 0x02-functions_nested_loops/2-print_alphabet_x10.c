035cc73d55a8501ff486099fa6c1821495473e18
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-20 06:44:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * Return : On sucess 1.
+ */
+void print_void_x10(void)
+{
+	char j, r
+	
+	for (r = 0; r < 10; r++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	}
+}	
