741afcc711275566413666abf9e2d79dcdf7a20e
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-20 07:57:20 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * _isalpha - checks for alphabetic character
- * @c: character to check if it is a lowercase
- * Return 1 if lowercase, 0 if not.
+ * _isalpha - check if character is lowercase or uppercase
+ * @c: character to check if it islowercase.
+ * Return: 1 if lowercase/uppercase, 0 if not.
  */
+
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c >= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
 	else
-	{return (0);
+	{
+		return (0);
 	}
 }
