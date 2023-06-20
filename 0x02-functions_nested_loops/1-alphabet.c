ea7b6dc94e4989918d888e9e7da724f14572b7d2
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-20 06:16:22 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print all lowercase alphabet
+ * Return: On success 1.
+ */
+void print_alphabet(void)
+{
+	char j;
+
+	for (j ='a'; j,='z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}	
