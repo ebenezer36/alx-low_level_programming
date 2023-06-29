1e476644831adbc8e2b1b1c66c3112d85a578841
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-29 04:53:21 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - print a function that change all lowercase of a string to uppercase
+ * @j : pointer
+ * Return: pointer in return
+ */
+char *string_toupper(char *j)
+{
+	int i;
+
+	for (i = 0; j[i] != 0; i++)
+	{
+		if (j[i] > 90 && j[i] != 10)
+		{
+			j[i] = j[i] - 32
+		}
+	}
+	return(j);
+}
