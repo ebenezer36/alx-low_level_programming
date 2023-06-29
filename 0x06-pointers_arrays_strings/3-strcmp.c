a752de3b39486ff6187afedf0f01a62a8ea217dc
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-29 06:31:57 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,23 @@
 #include "main.h"
 /**
- * _strcmp - function comparison of strings
- * @s1: pointer
- * @s2: pointer
- * Return: m-integer
+ *_strcmp - function comparison of strings
+ *@s1:  pointer
+ *@s2:  pointer
+ *Return: j-integer
  */
 int _strcmp(char *s1, char *s2)
 {
+	int i, j;
+
+	j = 0;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-        int i, m;
-
-        m = 0
-
-                for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-                {
-                        if (s1[i] != s2[i])
-                {
-                                m = s1[i] - s2[i];
-                                break;
-                }
-                }
-        return (m);
+		if (s1[i] != s2[i])
+	{
+		j = s1[i] - s2[i];
+		break;
+	}
+	}
+		return (j);
 }
