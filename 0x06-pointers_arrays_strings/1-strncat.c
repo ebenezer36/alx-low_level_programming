762b39623db11ef75053e00a5e165517160aa924
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-29 05:58:40 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,15 +16,15 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i] != '\0')
 	{
-		i++:
+		i++;
 	}
 
 	j = 0;
 	while (src[j] != '\0' && j < n)
 	{
-		dest[i] = src[j];
-		i+++;
-		j++;
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
 	dest[i] = '\0';
 
