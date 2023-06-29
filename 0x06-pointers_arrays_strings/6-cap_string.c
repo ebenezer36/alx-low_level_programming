6ea58fdbcefc3985f95bd184c2ce0a09858f681d
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-06-29 05:17:42 -0700

    6-cap-string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * cap_string - print a function that capitalize all the words of a string
+ * @j : pointer
+ * Return: Always 0.
+ */
+
+char *cap_string(char *j)
+{
+	int symb[14] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}',};
+	int i, k;
+
+	for (i = 0; j[i] != '\0'; i++)
+	{
+		if (j[0] >= 97 && j[0] <+ 122)
+		{
+			j[0] = j[0] - 32;
+		}
+		for (k = 0; k < 14; k++)
+			if (j[i] >= 97 && j[i] <= 122 && j[i - 1] == symb[k])
+				j[i] = j[i] - 32;
+	}
+	return (j);
+}
