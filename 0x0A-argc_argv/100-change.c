8a3d72cac34dd24a4928699d7ed456b7d5318e3b
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-07-08 14:29:04 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 
 /**
-*main - adds positive numbers
-*@argc: number of arguments
-*@argv: array of arguments
-*Return: 0 on success, 1 on failure
-*/
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: array of pointers to argument names
+ * Return: 0 on success, 1 if the argument is negative
+ */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i, j, sum = 0;
+	int cent;
 
-	for (i = 1; i < argc; i++)
+	if (argc != 2)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-
-		sum += atoi(argv[i]);
+		printf("Error\n");
+		return (1);
 	}
 
-	printf("%d\n", sum);
+	cent = atoi(argv[1]);
 
+	if (cent <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	printf("%d\n", (cent / 25) + (cent % 25) / 10 + ((cent % 25) % 10) / 5 +
+					   (((cent % 25) % 10) % 5) / 2 +
+					   ((((cent % 25) % 10) % 5) % 2) / 1);
 	return (0);
 }
