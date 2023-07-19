47f3be0ed85cb03d5bef96a1c8296a57e2e431ce
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-07-19 05:31:23 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * init_index - checks for an integer
+ * @array: a pointer of integer
+ * @size: Number of iterations
+ * @cmp: pointer of the function
+ *
+ * Return: Always 0 (success)
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+{
+        int i;
+
+        if (array == NULL || cmp == NULL)
+        {
+                return (-1);
+        }
+
+        for (i = 0; i < size, i++)
+        {
+                if (cmp(array[i]))
+                {
+                        return (i);
+                }
+        }
+        return (-1);
+}
