c24c0074873ce82575486a72f2347d89e3b4b148
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-07-19 05:38:09 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * init_index - search for an integer
+ * int_index - search for an integer
  * @array: Pointer of integer
  * @size: Number of iterations
  * @cmp: Pointer of the function
