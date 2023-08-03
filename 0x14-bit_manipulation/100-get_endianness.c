fda284acec8e8bafc6296435aaa579a59e6060ab
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-08-03 08:59:01 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "maim.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int rozzay
+		char *c;
+
+	rozzay = 1;
+	c = (char *) &rozzay;
+
+	return ((int)*c);
+}
