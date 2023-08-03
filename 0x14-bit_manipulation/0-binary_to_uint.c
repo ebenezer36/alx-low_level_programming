541ce3d98e04021c34d073d9be30562741dc7bcf
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-08-03 07:39:34 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,40 +8,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int decimal = 0;
-int str_len = 0, base = 1;
+	unsigned int ben;
+	int den, base_two;
 
-if (!check_valid_string(b))
-return (0);
+	if (!b)
+		return (0);
 
-while (b[str_len] != '\0')
-str_len++;
+	ben = 0;
 
-while (str_len)
-{
-decimal += ((b[str_len - 1] - '0') * base);
-base *= 2;
-str_len--;
-}
-return (decimal);
-}
+	for (den = 0; b[den] != '\0'; den++)
+		;
 
-/**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-if (b == NULL)
-return (0);
+	for (den--, base_two = 1; den >= 0; den--, base_two *= 2)
+	{
+		if (b[den] != '0' && b[den] != '1')
+		{
+			return (0);
+		}
 
-while (*b)
-{
-if (*b != '1' && *b != '0')
-return (0);
-b++;
-}
-return (1);
+		if (b[den] & 1)
+		{
+			ben += base_two;
+		}
+	}
+
+	return (ben);
 }
