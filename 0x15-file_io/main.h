c5f72d2d3099e043eb6d06f49553b63021aa0aff
ebenezer36 <ebenezerasabireadu55@gmail.com>
2023-08-07 14:14:42 -0700

    MAIN_H

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *gen_err(char *filename, int type);
+void error_from_to(char *filename, int type);
+void error_close(int filedescr);
 
-#endif /** MAIN_H */
+#define USAGE_ERR "Usage : cp file_from file_to\n"
+#define FROM_ERR "Error: Can't read from file "
+#define WRITE_ERR "Error: Can't write to "
+#define CLOSE_ERR "Error: Can't close "
+#define BUFF_SIZE 1024
+
+#endif /* MAIN_H */
